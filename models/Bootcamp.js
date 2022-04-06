e23ee0a4a40a821998237c1ac0a35068477a4971
David Vekony <dave.vekony@gmail.com>
2022-04-06 11:37:37 +0100

    implement slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -100,6 +101,14 @@ const BootcampSchema = new mongoose.Schema({
     type: mongoose.Schema.ObjectId,
     ref: "User",
   },
+});
+
+// Create bootcamp slug from the name
+// Don't use arrow function here because that handles the scope of 'this' differently
+// This is a Mongoose middleware/hook setup: https://mongoosejs.com/docs/middleware.html
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
